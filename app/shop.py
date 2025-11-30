0e5ae243c569b683ee0d2eaf48e768db6ef25b79
Anton Tsikhanovich <alluspeh@gmail.com>
2025-11-30 22:49:23 +0100

    Solution

--- app/shop.py
+++ app/shop.py
@@ -1,28 +1,58 @@
-from __future__ import annotations
-import datetime
+from app.configuration import config
+from app.customers import Customer
+from app.shop import Shop
 
 
-class Shop:
-    def __init__(self, name: str, location: list, products: dict) -> None:
-        self.name = name
-        self.location = location
-        self.products = products
+def shop_trip() -> None:
+    customers = [Customer(**customer_data) for
+                 customer_data in config["customers"]]
+    shops = [Shop(shop_data["name"],
+                  shop_data["location"], shop_data["products"])
+             for shop_data in config["shops"]]
 
-    def receipt(self, customer: str) -> None:
-        now = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        print(f"\nDate: {now}")
-        print(f"Thanks, {customer.name}, for your purchase!")
-        print("You have bought:")
+    for customer in customers:
+        print(f"{customer.name} has {customer.money} dollars")
 
-        total = 0
-        for product_name, amount in customer.product_cart.items():
-            cost = self.products[product_name] * amount
-            total += cost
+        trip_prices = {}
 
-            # formatowanie liczb
-            cost_str = f"{cost:.2f}".rstrip("0").rstrip(".")
-            print(f"{amount} {product_name}s for {cost_str} dollars")
+        for shop in shops:
+            cost = customer.shopping_cost_in(shop)
+            if cost is not None:
+                cost_str = f"{cost:.2f}".rstrip("0").rstrip(".")
+                if shop.name.startswith("Shop '"):
+                    print(f"{customer.name}'s "
+                          f"trip to {shop.name} costs {cost_str}")
+                else:
+                    print(f"{customer.name}'s trip "
+                          f"to the {shop.name} costs {cost_str}")
+                trip_prices[shop] = cost
 
-        total_str = f"{total:.2f}".rstrip("0").rstrip(".")
-        print(f"Total cost is {total_str} dollars")
-        print("See you again!\n")
+        if not trip_prices:
+            print(
+                f"{customer.name} doesn't have enough "
+                f"money to make a purchase in "
+                "any shop"
+            )
+            continue
+
+        shop_to_go = min(trip_prices, key=trip_prices.get)
+        min_cost = trip_prices[shop_to_go]
+
+        if customer.money < min_cost:
+            print(
+                f"{customer.name} doesn't have "
+                f"enough money to make a purchase in "
+                "any shop"
+            )
+            continue
+
+        print(f"{customer.name} rides to {shop_to_go.name}")
+        customer.location = shop_to_go.location[:]
+
+        shop_to_go.receipt(customer)
+
+        print(f"{customer.name} rides home")
+        customer.money -= min_cost
+        customer.money = round(customer.money, 2)
+        money_str = f"{customer.money:.2f}".rstrip("0").rstrip(".")
+        print(f"{customer.name} now has {money_str} dollars\n")
